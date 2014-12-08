bbdc0ea5ca300dca42418d55693b1cd8cbb99e53
Jess Scheuring <jscheuring@gmail.com>
2014-12-08 01:03:06 -0500

    adds embedded code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('The Iron Yard Rocks');
+(function() {
+  'use strict';
+
+  var codeBlock = {
+
+    format: function($el) {
+      var $h2 = $el.siblings('h2'),
+          $p = $el.siblings('p'),
+          h = 400 - $h2.outerHeight(true) - $p.outerHeight(true);
+      $el.height(h);
+    },
+
+    populate: function($el) {
+      var id = $el.attr('id');
+      $.get('../scripts/' + id + '.pde', function(data) {
+        $el.html(data);
+      });
+    }
+
+  };
+
+  _.each($('.prettyprint'), function(obj) {
+    codeBlock.format($(obj));
+    codeBlock.populate($(obj));
+  });
+
+})();
